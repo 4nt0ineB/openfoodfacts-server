f55fa774ca6a2f6e37ba81074a42844f0c1aea9c
Antoine B <antoine.bastos@edu.univ-eiffel.fr>
2024-07-26 09:25:17 +0200

    use iframe

--- cgi/content.pl
+++ cgi/content.pl
@@ -12,38 +12,31 @@ use Storable qw/dclone/;
 use Log::Any qw($log);
 use Encode;
 
+sub format_page {
+    my ($content, $request_ref) = @_;
 
+    my $text_lc = $request_ref->{lc};
 
+    $request_ref->{styles} .= '';
+    $request_ref->{header} .= '';
 
+    $request_ref->{title} = "";
 
-
-
-
-# sub format_page {
-#     my ($content, $request_ref) = @_;
-
-#     my $text_lc = $request_ref->{lc};
-
-#     $request_ref->{styles} .= '';
-#     $request_ref->{header} .= '';
-
-#     $request_ref->{title} = "";
-
-#     # https://s0.wp.com/wp-content/plugins/gutenberg-core/v18.8.0/build/block-library/style.css?m=1721328021i&ver=18.8.0
-#     my $html = "";
-#     $html = '<link rel="stylesheet" href="'.$static_subdomain.'/css/wp.css"></style>';
-#     $html .= <<"TITLE";
-#     <div class="wp-block-group has-global-padding is-layout-constrained wp-block-group-is-layout-constrained">
-# 		<div style="height:var(--wp--preset--spacing--50)" aria-hidden="true" class="wp-block-spacer"></div>
-# 		<h1 class="has-text-align-center wp-block-post-title">$content->{title}->{rendered}</h1>
-# 		<div style="margin-top:0;margin-bottom:0;height:var(--wp--preset--spacing--30)" aria-hidden="true" class="wp-block-spacer"></div>	
-# 	</div>
-# TITLE
+    # https://s0.wp.com/wp-content/plugins/gutenberg-core/v18.8.0/build/block-library/style.css?m=1721328021i&ver=18.8.0
+    my $html = "";
+    $html = '<link rel="stylesheet" href="'.$static_subdomain.'/css/wp.css"></style>';
+    $html .= <<"TITLE";
+    <div class="wp-block-group has-global-padding is-layout-constrained wp-block-group-is-layout-constrained">
+		<div style="height:var(--wp--preset--spacing--50)" aria-hidden="true" class="wp-block-spacer"></div>
+		<h1 class="has-text-align-center wp-block-post-title">$content->{title}->{rendered}</h1>
+		<div style="margin-top:0;margin-bottom:0;height:var(--wp--preset--spacing--30)" aria-hidden="true" class="wp-block-spacer"></div>	
+	</div>
+TITLE
     
-#     $html .= "<h1 class=\"entry-title\"></h1>";
-#     $html .= "<div class=\"entry-content wp-block-post-content has-global-padding is-layout-constrained wp-block-post-content-is-layout-constrained\">$content->{content}->{rendered}</div>";
+    $html .= "<h1 class=\"entry-title\"></h1>";
+    $html .= "<div class=\"entry-content wp-block-post-content has-global-padding is-layout-constrained wp-block-post-content-is-layout-constrained\">$content->{content}->{rendered}</div>";
 
-#     ${$request_ref->{content_ref}} =  $html;
+    ${$request_ref->{content_ref}} =  $html;
     $request_ref->{canon_url} = "/bop";
 }
 
@@ -52,6 +45,27 @@ my $template_data_ref = {};
 
 my $request_ref = ProductOpener::Display::init_request();
 # my $content = format_page(wp_get_page_by_id('14'), $request_ref);
+
+
+
+${$request_ref->{content_ref}} = <<HTML;
+
+<style>
+  iframe {
+    width: 100%;
+    height: 100vh;
+  }
+</style>
+
+<iframe id="myIframe" src="http://localhost:8080/hello-world" frameborder="0"></iframe>
+
+<script src="http://static.openfoodfacts.localhost/js/dist/iframe-resizer.parent.js"></script>
+<script>
+  iframeResize({ license: 'GPLv3', waitForLoad: true }, '#myIframe');
+</script>
+
+HTML
+
 
 
 display_page($request_ref);
